241eda9e3565e6bae9513e152aa99205ee1f3cb4
John Weland <john.weland@gmail.com>
2023-12-28 16:33:27 -0600

    Initial commit

---
+++ test/foundry-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FoundryEcs from '../lib/foundry-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/foundry-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FoundryEcs.FoundryEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
